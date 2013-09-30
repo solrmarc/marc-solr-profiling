8faec05d8ef588647fbddabcd749cac5b864d2ba
Naomi Dushay <ndushay@stanford.edu>
2013-09-30 15:40:07 -0700

    working on renames

---
+++ profiling/src/edu/stanford/enumValues/Access.java
@@ -0,0 +1,27 @@
+package edu.stanford.enumValues;
+
+/** 
+ * access facet values for Stanford University
+ * @author - Naomi Dushay
+ */
+public enum Access {
+	ONLINE,
+	AT_LIBRARY;
+
+	/**
+	 * need to override for text of multiple words
+	 */
+	@Override
+	public String toString() {
+		switch (this) {
+		case AT_LIBRARY:
+			return "At the Library";
+		case ONLINE:
+			return "Online";
+		}
+		String lc = super.toString().toLowerCase();
+		String firstchar = lc.substring(0, 1).toUpperCase();
+		return lc.replaceFirst(".{1}", firstchar);
+	}
+}
+
